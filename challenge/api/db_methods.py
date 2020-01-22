cd9a61539adbdb9ab449cb6a69c8367c22f3e5ec
gncrm <gncrm94@gmail.com>
2020-01-22 16:28:28 +0000

    Added the function to create occurrences

--- challenge/api/db_methods.py
+++ challenge/api/db_methods.py
@@ -1,9 +1,24 @@
 from api.models import Occurrence, User
 import secrets
+from django.utils import timezone
 
 class DbManager:
 
 	admin_password = 'ABCDEFGH'
+
+	def checkCategory(self, category):
+		valid_categories = ['CONSTRUCTION', 'SPECIAL_EVENT', 'INCIDENT', 'WEATHER_CONDITION', 'ROAD_CONDITION']
+		if category.upper() in valid_categories:
+			return 1
+		else:
+			return 0
+
+	def checkCoordinates(self, latitude, longitude):
+		if float(latitude) < -90.0 or float(latitude) > 90.0:
+			return 0
+		if float(longitude) < -180.0 or float(longitude) > 180.0:
+			return 0
+		return 1
 
 	def registerUser(self, username):
 		try:
@@ -27,3 +42,20 @@ class DbManager:
 				return u
 			else:
 				return -2
+
+	#occ_id, description, latitude, longitude, user, creation_date, update_date, status, category
+
+	def addOccurrence(self, description, latitude, longitude, user_token, category):
+		try:
+			user = User.objects.get(api_token = user_token)
+			if self.checkCoordinates(latitude, longitude) and self.checkCategory(category):
+				occ = Occurrence(description = description, latitude = latitude, longitude = longitude,
+					user = user, category = category.upper())
+				occ.creation_date = timezone.now()
+				occ.status = 'unvalidated'
+				occ.save()
+				return occ
+			else:
+				return None
+		except User.DoesNotExist:
+			return -1
--- challenge/api/views.py
+++ challenge/api/views.py
@@ -9,6 +9,8 @@ from rest_framework.response import Response
 from api.db_methods import DbManager 
 # Create your views here.
 
+#Admin do sistema: da5fb0fa317f84a06990ccc5e71864f7
+
 class UserRegistration(APIView):
 
 	def post(self, request, format = None):
@@ -24,3 +26,26 @@ class UserRegistration(APIView):
 				return JsonResponse({}, status = 200)
 		else:
 			return JsonResponse({}, status = 200)
+
+class OccurrenceList(APIView):
+
+	#[occ_id], description, latitude, longitude, user, [creation_date], [update_date], [status], category
+
+	def get(self, request, format = None):
+		db = DbManager()
+		pass
+
+	def post(self, request, format = None):
+		db = DbManager()
+		required_args = ['description', 'latitude', 'longitude', 'user_token', 'category']
+		for req_arg in required_args:
+			if req_arg not in request.query_params:
+				return JsonResponse({}, status = 200)
+		res = db.addOccurrence(request.query_params['description'], request.query_params['latitude'], 
+			request.query_params['longitude'], request.query_params['user_token'], request.query_params['category'])
+		if res == None:
+			return JsonResponse({}, status = 200)
+		elif res == -1:
+			return JsonResponse({}, status = 200)
+		else:
+			return Response(OccurrenceSerializer(res).data)
