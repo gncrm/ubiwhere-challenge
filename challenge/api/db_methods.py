c6f099c7575c01d6d4ef305ccace338b0dfb3bb8
gncrm <gncrm94@gmail.com>
2020-01-23 12:00:19 +0000

    Small fixes. Added comments to the code

--- challenge/api/db_methods.py
+++ challenge/api/db_methods.py
@@ -4,8 +4,13 @@ from django.utils import timezone
 
 class DbManager:
 
+	'''The admin password is a code that admins must enter at account creation to be granted admin status'''
 	admin_password = 'ABCDEFGH'
 
+	'''
+	This function checks if a category (argument) is one of the categories accepted by the API
+	It converts the category it receives to uppercase for a better comparison
+	'''
 	def checkCategory(self, category):
 		valid_categories = ['CONSTRUCTION', 'SPECIAL_EVENT', 'INCIDENT', 'WEATHER_CONDITION', 'ROAD_CONDITION']
 		if category.upper() in valid_categories:
@@ -13,13 +18,26 @@ class DbManager:
 		else:
 			return 0
 
+	'''
+	This function checks if a location is valid, that is, if it is numeric and if -90 < latitude < 90 and -180 < longitude < 180
+	'''
 	def checkCoordinates(self, latitude, longitude):
+		try:
+			float(latitude)
+			float(longitude)
+		except:
+			return 0
 		if float(latitude) < -90.0 or float(latitude) > 90.0:
 			return 0
 		if float(longitude) < -180.0 or float(longitude) > 180.0:
 			return 0
 		return 1
 
+	'''
+	This function registers non admin users so they can use the API. It receives the desired username
+	If the username already exists, it returns the proper error code
+	If the username is available, it creates the database entry for the user, with a generated API token ,and returns the user information
+	'''
 	def registerUser(self, username):
 		try:
 			user = User.objects.get(pk = username)
@@ -30,6 +48,11 @@ class DbManager:
 			u.save()
 			return u
 
+	'''
+	This function registers admins. It receives the desired username and the admin password to verify the admin status
+	If the username already exists or the password is wrong it returns de appropriate error code
+	If everything is correct, the database entry is created and the admin information is returned
+	'''
 	def registerAdmin(self, username, pw):
 		try:
 			user = User.objects.get(pk = username)
@@ -43,6 +66,12 @@ class DbManager:
 			else:
 				return 400
 
+	'''
+	This function creates new occurrences. It receives the description, location, token of the creator and cateogry
+	If the token does not exist, the coordinates are not valid or the cateogry is not valid, the appropriate error code is returned
+	If everything is correct, the occurrence entry is created, with an unvalidated status and the current date and time as the creation date
+	The information of the occurrence is then returned
+	'''
 	def addOccurrence(self, description, latitude, longitude, user_token, category):
 		try:
 			user = User.objects.get(api_token = user_token)
@@ -58,9 +87,16 @@ class DbManager:
 		except User.DoesNotExist:
 			return 401
 
+	'''
+	This function returns the list of all occurrences in the database, ordered from the most recent to the oldest
+	'''
 	def getOccurrenceList(self):
 		return Occurrence.objects.all().order_by('-creation_date')
 
+	'''
+	This function returns information about the occurrence with the ID equal to the one received as argument
+	If the occurrence does not exist the appropriate error code is returned
+	'''
 	def getOccurrenceDetails(self, pk):
 		try:
 			occ = Occurrence.objects.get(occ_id = pk)
@@ -68,6 +104,12 @@ class DbManager:
 		except Occurrence.DoesNotExist:
 			return 404
 
+	'''
+	This function updates the status of occurrences. It receives the ID and the token of the user trying to update it
+	If the user is not an admin, the occurrence does not exist or its status is already solved, the appropriate error code is returned
+	If everything is correct, the status is updated (unvalidated to validated or validated to solved) and the date of update is set to the current date and time
+	The information of the occurrence is then returned
+	'''
 	def changeOccurrenceStatus(self, pk, user_token):
 		try:
 			user = User.objects.get(api_token = user_token)
@@ -91,15 +133,26 @@ class DbManager:
 		except User.DoesNotExist:
 			return 401
 
+	'''
+	This function returns the list of occurrences created by the user with the username received as argument, ordered from the most recent to the oldest
+	'''
 	def filterOccurrencesByUser(self, username):
 		return Occurrence.objects.filter(user = username).order_by('-creation_date')
 
+	'''
+	This function returns the list of occurrences with the category received as argument, ordered from the most recent to the oldest
+	If the category is invalid, the proper error code is returned
+	'''
 	def filterOccurrencesByCategory(self, categ):
 		if self.checkCategory(categ):
 			return Occurrence.objects.filter(category = categ.upper()).order_by('-creation_date')
 		else:
 			return 400
 
+	'''
+	This function returns the list of occurrences within radius of the location received as argument, ordered from the most recent to the oldest
+	If the location is invalid or the radius is negative, the proper error code is returned
+	'''
 	def filterOccurrencesByLocation(self, latitude, longitude, radius):
 		if self.checkCoordinates(latitude, longitude) and radius >= 0:
 			pass
--- challenge/api/views.py
+++ challenge/api/views.py
@@ -14,8 +14,19 @@ from django.template import loader
 resp_codes = {200: status.HTTP_200_OK, 201: status.HTTP_201_CREATED, 400: status.HTTP_400_BAD_REQUEST, 
 	401: status.HTTP_401_UNAUTHORIZED, 409: status.HTTP_409_CONFLICT, 404: status.HTTP_404_NOT_FOUND}
 
+'''
+This class is responsible for handling requests for user registration
+api/user-registration/
+'''
 class UserRegistration(APIView):
 
+	'''
+	This method handles a POST to register a user
+	If the request does not contain a username, the appropriate error code is returned
+	If it contains a username and the admin password, an attempt is made to create a new admin
+	If it contains only the username, an attempt is made to create a new user
+	The appropriate error code or the data of the created account are returned, depending on the success of the account creation
+	'''
 	def post(self, request, format = None):
 		db = DbManager()
 		if 'username' in request.query_params:
@@ -30,8 +41,19 @@ class UserRegistration(APIView):
 		else:
 			return Response(status=resp_codes[400])
 
+'''
+This class is responsible for handling requests at api/occurrences/
+'''
 class OccurrenceList(APIView):
 
+	'''
+	This function handles GET requests
+	If the request has no parameters, then all the occurrences in the database are returned
+	If the request has a username, then all the occurrences created by that user are returned
+	If the request has a category, an attempt is made to list all occurrences of that category
+	If the request has a location and a radius, an attempt is made to list all the occurrences in range of that location
+	If there are no valid parameters or the request generates an error, the appropriate error code is returned
+	'''
 	def get(self, request, format = None):
 		db = DbManager()
 		if len(request.query_params) == 0:
@@ -52,6 +74,11 @@ class OccurrenceList(APIView):
 			else:
 				return Response(OccurrenceSerializer(res, many = True).data, status=resp_codes[200])
 
+	'''
+	This function handles POST requests
+	If the request has the correct parameters to create a new occurrence, an attempt is made to do so, and the result or error code resulting are returned
+	If not, the appropriate error code is returned
+	'''
 	def post(self, request, format = None):
 		db = DbManager()
 		required_args = ['description', 'latitude', 'longitude', 'user_token', 'category']
@@ -65,8 +92,15 @@ class OccurrenceList(APIView):
 		else:
 			return Response(OccurrenceSerializer(res).data, status=resp_codes[201])
 
+'''
+This class is responsible for handling requests at api/occurrences/{id}/
+'''
 class OccurrenceDetail(APIView):
 
+	'''
+	This function handles GET requests
+	It returns either the information of the occurrence with the ID in the URL, or the appropriate error code
+	'''
 	def get(self, request, pk, format = None):
 		db = DbManager()
 		res = db.getOccurrenceDetails(pk)
@@ -75,6 +109,11 @@ class OccurrenceDetail(APIView):
 		else:
 			return Response(OccurrenceSerializer(res).data, status=resp_codes[200])
 
+	'''
+	This function handles PUT requests
+	If the request has the token of the user, an attempt is made to update the status of the occurrence with the ID in the URL and the result is returned
+	If not, the appropriate error code is returned
+	'''
 	def put(self, request, pk, format = None):
 		db = DbManager()
 		if 'user_token' in request.query_params:
@@ -86,6 +125,9 @@ class OccurrenceDetail(APIView):
 		else:
 			return Response(status=resp_codes[401])
 
+'''
+This method returns the HTML page that contains the description of the API and its endpoints, at api/
+'''
 def api_help(request):
 	template = loader.get_template('api/api_help.html')
 	return HttpResponse(template.render())
